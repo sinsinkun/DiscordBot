d09321a85f6c165bd60a4275db410f753cbea802
NJRodriguez <nicolasjrodriguez1@gmail.com>
2020-07-14 20:09:03 -0300

    Poll customEmojis and 50 messages from server

--- index.js
+++ index.js
@@ -8,9 +8,25 @@ client.once('ready', () => {
 
 client.login(token);
 
-client.on('message', message => {
-	if (message.author.bot) return;
-	if (message.content == "<:sip:701932618110992395>") {
-		message.channel.send("<:sip:701932618110992395>")
+client.on('message', async message => {
+    if (message.content == `${prefix}emojiUsage`){
+		message.channel.send("WIP");
+		if (message.guild.available && message.channel.type === "text") {
+			let customEmojis = getCustomEmojis(message);
+			message.channel.send(`You degenerates are using ${customEmojis.length} custom emojis!`);
+			let postHistory = await getPostHistory(message);
+			message.channel.send(`You degenerates have ${postHistory.size} posts up to now!`)
+		}
 	}
 });
+
+function getCustomEmojis(message) {
+	let customEmojis = message.guild.emojis.cache.map(emoji => {
+		return `:${emoji.name}:${emoji.id}`;
+	})
+	return customEmojis;
+}
+
+async function getPostHistory(message) {
+	return message.channel.messages.fetch()
+}
