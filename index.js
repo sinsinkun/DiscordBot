d911840c5842643bef20b1dacc3749e0afdd0023
Sinsinkun <returntosender1020@hotmail.com>
2020-07-14 15:45:42 -0400

    First Upload
    no modules folder

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+//client.login(NzMyNjQ4NjQ5NDA1NzU5NTE5.Xw4EYA.HeMaF_b-UosxZM5HWbA1Y8xcDyo);
