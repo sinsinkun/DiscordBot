c324da29e35df12c9b5dbbb59ef3207bf3866f8a
NJRodriguez <nicolasjrodriguez1@gmail.com>
2020-07-14 17:59:36 -0300

    Add rudimentary security through a config.json file

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json')
 const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-//client.login(#securitytoken#);
+client.login(token);
+
+client.on('message', message => {
+	if (message.author.bot) return;
+	if (message.content == "<:sip:701932618110992395>") {
+		message.channel.send("<:sip:701932618110992395>")
+	}
+});
