cea6daaaf4caa9adfa467acfc4070637f2aa3e2c
NJRodriguez <nicolasjrodriguez1@gmail.com>
2020-07-21 20:57:13 -0300

    Fix server emojiusage command

--- src/commands/emojiusage.js
+++ src/commands/emojiusage.js
@@ -1,22 +1,13 @@
 const name = 'emoji usage';
-const description = 'Tallies top 50 emojis used over the last 6 months';
-const { getPostHistory } = require('../helpers/posts')
-const emojis = require('../helpers/emojis')
-const DiscordUser = require('../common/data/user')
+const description = 'Tallies top 25 emojis of all time for server or @user.';
 
-async function emojiUsage({ message, timeInEpoch, args, discordData }) {
+async function emojiUsage({ message, args, discordData }) {
     if (message.guild.available && message.channel.type === "text" && args.length === 0) {
-        const customEmojis = emojis.getCustomEmojis(message);
-        message.channel.send(`You degenerates are using ${customEmojis.length} custom emojis!`);
-        const postHistory = await getPostHistory(message, timeInEpoch);
-        message.channel.send(`You degenerates have ${postHistory.length} posts since half an year ago!`);
-        const emojisOrderedByUsage = emojis.tabulateEmojis(customEmojis, postHistory);
-        message.channel.send(emojisOrderedByUsage);
+        const emojisUsed = await discordData.server.getEmojiUsage();
+        message.channel.send(emojisUsed);
     }
     else if (message.mentions.users.size === 1) {
-        const {id, username} = message.mentions.users.first();
-        const user = new DiscordUser(id, username, message.guild.name);
-        const emojisUsed = await user.getEmojiUsage();
+        const emojisUsed = await discordData.user.getEmojiUsage();
         message.channel.send(emojisUsed);
     }
     else {
