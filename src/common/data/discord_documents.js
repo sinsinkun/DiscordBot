fa6561d30efb85a1834234ddedb8ffbdcb20020a
NJRodriguez <nicolasjrodriguez1@gmail.com>
2021-03-09 16:34:16 -0300

    Remove historical emoji usage

--- src/common/data/discord_documents.js
+++ src/common/data/discord_documents.js
@@ -1,5 +1,4 @@
 const db = require('../clients/dynamodb');
-const { getPostHistory } = require('../../helpers/posts')
 const emojis = require('../../helpers/emojis');
 const { occurrences } = require('../../helpers/external');
 
@@ -12,10 +11,6 @@ class DiscordDocument {
 
     async create(document, message) {
         await this._db.create(document)
-        const history = await getPostHistory(message);
-        const customEmojis = await emojis.getCustomEmojis(message);
-        const emojiUsage = countEmojiUse(customEmojis, history);
-        await this.logHistoricEmojiUsage(emojiUsage)
     }
 
     async confirmExistence() {
@@ -29,42 +24,6 @@ class DiscordDocument {
             throw new Error("This user hasn't used any custom emojis!");
         }
         return array;
-    }
-
-    async logHistoricEmojiUsage(historicEmojiUsage) {
-        historicEmojiUsage.forEach(async emoji => {
-            const updateExpression = "SET #emojiUsage.#emoji = #emojiUsage.#emoji + :increment"
-            const createExpression = "SET #emojiUsage.#emoji = if_not_exists(#emojiUsage.#emoji, :increment)"
-            const expressionNames = {
-                '#emojiUsage': "emojiUsage",
-                '#emoji': Object.name(emoji),
-            }
-            const expressionValues = {
-                ':increment': emoji,
-            }
-            const params = {
-                TableName: this._tableName,
-                Key: {
-                    'id': this._id,
-                },
-                ExpressionAttributeNames: expressionNames,
-                ExpressionAttributeValues: expressionValues,
-            }
-            try {
-                await this._db.update({...params, UpdateExpression: updateExpression });   
-            } catch (error) {
-                if (error.name === "ValidationException") {
-                    // Update to existing emoji failed, probably doesn't exist.
-                    try {
-                        // Create new emoji in list with increment value.
-                        await this._db.update({...params, UpdateExpression: createExpression })         
-                    } catch (error) {
-                        console.log(error);
-                    }
-                }
-                else throw error;
-            }
-        })
     }
 
     async logEmojiUsage(message) {
@@ -106,18 +65,6 @@ class DiscordDocument {
             }
         })
     }
-}
-
-function countEmojiUse(emojis, postHistory) {
-	postHistory.forEach(message => {
-		emojis.forEach(emoji => {
-			const count = occurrences(message.content, Object.keys(emoji)[0], false);
-			if (count >= 1){
-				emoji += count > 3 ? 3 : count;
-			}
-		})
-	})
-	return emojis;
 }
 
 module.exports = DiscordDocument
